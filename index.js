2cf0902bec0ee9c8d072312eb1a9de20846f5aeb
Nikita <nubovik01@gmail.com>
2023-02-12 02:12:42 +0300

    quotes в отдельном файле

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 const express = require("express");
 const bodyParser = require('body-parser');
+let quotes=require('./quotes.json');
 
 const app = express();
 app.use(bodyParser.json());
 
-let items=[
-  {
-    "id": 0
-  }
-]
-
 app.get("/", async function (req, res) {
-  return res.send(items[Math.floor(Math.random() * items.length)]);
+  return res.send(quotes[Math.floor(Math.random() * quotes.length)]);
 });
 
 app.listen(process.env.PORT || 21790, function () {
