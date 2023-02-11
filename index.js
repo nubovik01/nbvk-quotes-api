9268f4474513c9c57c456d334143cc2878661c8a
Nikita <nubovik01@gmail.com>
2023-02-12 02:09:21 +0300

    Рабочее API готово!

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 const express = require("express");
 const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+let items=[
+  {
+    "id": 0
+  }
+]
+
+app.get("/", async function (req, res) {
+  return res.send(items[Math.floor(Math.random() * items.length)]);
+});
+
+app.listen(process.env.PORT || 21790, function () {
+  console.log('Express listening on %s:%s', this.address().address, this.address().port);
+});
