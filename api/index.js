212b71c794876a9d7ff1c25ec2186db6dbcfcea2
Nikita <nubovik01@gmail.com>
2023-02-12 04:29:31 +0300

    обратно

--- api/index.js
+++ api/index.js
@@ -9,6 +9,10 @@ app.get("/", async function (req, res) {
   return res.send({ "comment": "go to /v1" });
 });
 
+app.get("/v1", async function (req, res) {
+    return res.send(quotes[Math.floor(Math.random() * quotes.length)]);
+  });
+
 app.listen(process.env.PORT || 21790, function () {
   console.log('Express listening on %s:%s', this.address().address, this.address().port);
 });
