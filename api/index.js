4b66f564fbecc856c59a762b60d86f39a5de1b91
Nikita <nubovik01@gmail.com>
2023-02-12 04:31:30 +0300

    format code

--- api/index.js
+++ api/index.js
@@ -10,8 +10,8 @@ app.get("/", async function (req, res) {
 });
 
 app.get("/v1", async function (req, res) {
-    return res.send(quotes[Math.floor(Math.random() * quotes.length)]);
-  });
+  return res.send(quotes[Math.floor(Math.random() * quotes.length)]);
+});
 
 app.listen(process.env.PORT || 21790, function () {
   console.log('Express listening on %s:%s', this.address().address, this.address().port);
