ceff6b2c264617d5d91664c8e50e16d103474928
Nikita <nubovik01@gmail.com>
2023-02-12 04:08:15 +0300

    format code

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const bodyParser = require('body-parser');
-let quotes=require('./quotes.json');
+let quotes = require('./quotes.json');
 
 const app = express();
 app.use(bodyParser.json());
 
 app.get("/", async function (req, res) {
-  return res.send({"comment": "go to /v1"});
+  return res.send({ "comment": "go to /v1" });
 });
 
 app.get("/v1", async function (req, res) {
