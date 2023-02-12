e3ef6e28b0c5b399bdf0b80418fb9fb66dbc4ef9
Nikita <nubovik01@gmail.com>
2023-02-12 04:28:08 +0300

    v1

--- api/v1/index.js
+++ api/v1/index.js
@@ -5,10 +5,10 @@ let quotes = require('../quotes.json');
 const app = express();
 app.use(bodyParser.json());
 
-app.get("/", async function (req, res) {
-  return res.send({ "comment": "go to /v1" });
+app.get("/v1", async function (req, res) {
+  return res.send(quotes[Math.floor(Math.random() * quotes.length)]);
 });
 
-app.listen(process.env.PORT || 21790, function () {
+app.listen(process.env.PORT || 21791, function () {
   console.log('Express listening on %s:%s', this.address().address, this.address().port);
 });
