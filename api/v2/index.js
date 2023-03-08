9548c18a37c67f960f378d077cc2793f17d60424
Nikita <nubovik01@gmail.com>
2023-03-09 02:53:48 +0300

    переписал код и +возможность вывода архивных цитат

--- api/v2/index.js
+++ api/v2/index.js
@@ -7,40 +7,33 @@ let sources = require('../sources.json');
 const app = express();
 app.use(bodyParser.json());
 
-app.get("/v2", async function (req, res) {
-  let quote = quotes[Math.floor(Math.random() * quotes.length)];
-  let SourceObj = sources[quote.source - 1];
+app.get("/v2", async (req, res) => {
+  let filteredQuotes = req.query.archived ? quotes : quotes.filter(quote => !quote.archived);
+  let quote = filteredQuotes[Math.floor(Math.random() * filteredQuotes.length)];
+  let source = sources.find(s => s.id === quote.source);
+  let authorsData = quote.authors.map(authorId => {
+    let { id, first_name, last_name, nickname, image } = authors.find(a => a.id === authorId);
+    return { id, first_name, last_name, nickname, image };
+  });
 
   let data = {
     id: quote.id,
     quote_text: quote.quote_text,
     age_restriction: quote.age_restriction,
     archived: quote.archived,
-    source: [
-      {
-        id: SourceObj.id,
-        age_restriction: SourceObj.age_restriction,
-        name: SourceObj.name,
-        link: SourceObj.link,
-        authors: []
-      }
-    ]
-  };
-
-  for (let AuthorId of quote.authors) {
-    let author=authors[AuthorId-1];
-    data.source[0].authors.push({
-      id: author.id,
-      first_name: author.first_name,
-      last_name: author.last_name,
-      nickname: author.nickname,
-      image: author.image
-    });
+    source: {
+      id: source.id,
+      age_restriction: source.age_restriction,
+      name: source.name,
+      link: source.link,
+      authors: authorsData
+    }
   };
 
   return res.send(data);
 });
 
-app.listen(process.env.PORT || 21792, function () {
-  console.log('Express listening on %s:%s', this.address().address, this.address().port);
+const port = process.env.PORT || 21792;
+app.listen(port, () => {
+  console.log(`Express listening on port ${port}`);
 });
